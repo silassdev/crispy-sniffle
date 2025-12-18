8fb6dfc1adf13e52b2f9bdcc1b054f91a6013ab1
Silas Tyokaha <3tyokaha@gmail.com>
2025-12-18 18:41:52 +0100

    removed cache

--- config/media-library.php
+++ config/media-library.php
@@ -1,3 +1,13 @@
-'perform_conversions_on_queue' => true,
+<?php
 
-'queue_name' => env('MEDIALIBRARY_QUEUE', 'media'),
+return [
+
+    'disk_name' => env('MEDIA_DISK', 'public'),
+
+    'max_file_size' => 1024 * 1024 * 10,
+
+    'perform_conversions_on_queue' => true,
+
+    'queue_name' => env('MEDIALIBRARY_QUEUE', 'media'),
+
+];
