e535227947e3c4df1897b32b097e7e2dff087682
Silas Tyokaha <3tyokaha@gmail.com>
2025-12-20 20:04:51 +0100

    removed cache

--- storage/framework/views/bb512f515700ebd30458bc6cc54e6734.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php $__env->startSection('content'); ?>
-  <h1>Blogs</h1>
- <livewire:community.community-feed post-type"blog" />
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-lms\resources\views/blogs.blade.php ENDPATH**/ ?>
